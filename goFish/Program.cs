160ee9db4ee2df52d6a4636941d637ee4a6499ff
EmilWallman <emil.wallman@elev.ga.ntig.se>
2022-09-21 08:48:03 +0200

    first push

---
+++ goFish/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace goFish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start question
+            bool start = startYN();
+
+            //Start if loop (the whole game)
+            if (start == true)
+            {
+
+                var tupleList = new List<(int, string)> 
+                { 
+                    
+                };
+
+
+
+
+                List<int> hand = new List<int>() { };
+                List<int> computerhand = new List<int>() { };
+
+
+
+
+
+
+
+
+
+            }
+
+
+            //If you dont want to play
+            else
+            {
+                Console.WriteLine("Oh... Okay... Bye then :(");
+            }
+
+
+
+        }
+
+        //start question
+        static bool startYN()
+        {
+            bool YN;
+
+            Console.WriteLine("Welcome to goFish do you want to play a game? Y/N");
+            string YNstring = Console.ReadLine();
+
+            if (YNstring == "Y" || YNstring == "y")
+            {
+                YN = true;
+                
+            }
+
+            else
+            {
+                YN = false;
+            }
+
+            return YN;
+        }
+        
+    }
+}
